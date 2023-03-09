246340724642781511c604e7970aaf14ff408c63
Kayky Morais <kaykygithub@gmail.com>
2023-03-09 12:25:51 -0300

    Front-End - React.JS

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+import './App.css';
+import React, { useState, useEffect, useCallback, useRef } from 'react'
+import { ChatClient } from './chat-client';
+
+const URL = 'wss://sjmvej6pb3.execute-api.us-east-2.amazonaws.com/production/';
+
+const App = () => {
+  const socket = useRef<WebSocket | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [members, setMembers] = useState([]);
+  const [chatRows, setChatRows] = useState<React.ReactNode[]>([]);
+  let name;
+
+  //Conexão com o Websocket e primeira interação com o usuario (setName)
+  const onSocketOpen = useCallback(() => {
+    setIsConnected(true);
+    name = prompt('Informe o seu nome:');
+    if (name !== null && name !== "") {
+      socket.current?.send(JSON.stringify({ action: 'setName', name }));
+    } else {
+      socket.current?.send(JSON.stringify({ action: 'setName', name:'Anônimo' }));
+    }
+  }, []);
+
+  //Desconexão com o WebSocket
+  const onSocketClose = useCallback(() => {
+    setMembers([]);
+    setIsConnected(false);
+    setChatRows([]);
+  }, []);
+
+  //Método para chamar as funções do WebSocket
+  const onSocketMessage = useCallback((dataStr) => {
+    const data = JSON.parse(dataStr);
+    if (data.members) {
+      setMembers(data.members);
+    } else if (data.publicMessage) {
+      setChatRows(oldArray => [...oldArray, <span><b>{data.publicMessage}</b></span>]);
+    } else if (data.privateMessage) {
+      alert(data.privateMessage);
+    } else if (data.systemMessage) {
+      setChatRows(oldArray => [...oldArray, <span><i>{data.systemMessage}</i></span>]);
+    }
+  }, []);
+
+  //Listeners do webSocket
+  const onConnect = useCallback(() => {
+    if (socket.current?.readyState !== WebSocket.OPEN) {
+      socket.current = new WebSocket(URL);
+      socket.current.addEventListener('open', onSocketOpen);
+      socket.current.addEventListener('close', onSocketClose);
+      socket.current.addEventListener('message', (event) => {
+        onSocketMessage(event.data);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      socket.current?.close();
+    };
+  }, []);
+
+  const onSendPrivateMessage = useCallback((to: string) => {
+    const message = prompt('Mensagem privada para: ' + to);
+    if (message !== null && message !== "" && to !== name) {
+      socket.current?.send(JSON.stringify({
+        action: 'sendPrivate',
+        message,
+        to,
+      }));
+    }
+  }, []);
+
+  const onSendPublicMessage = useCallback(() => {
+    const message = prompt('Mensagem pública');
+    if (message !== null && message !== "") {
+      socket.current?.send(JSON.stringify({
+        action: 'sendPublic',
+        message,
+      }));
+    }
+  }, []);
+
+  const onSendBotMessage = useCallback(() => { 
+    const message = prompt('Mensagem  para BOT ');
+    if (message !== null && message !== "") {
+      socket.current?.send(JSON.stringify({
+        action: 'sendBot',
+        message
+      }));
+    }
+   }, []);
+
+  const onDisconnect = useCallback(() => {
+    if (isConnected) {
+      socket.current?.close();
+    }
+  }, [isConnected]);
+
+  return <ChatClient
+    isConnected={isConnected}
+    members={members}
+    chatRows={chatRows}
+    onPublicMessage={onSendPublicMessage}
+    onPrivateMessage={onSendPrivateMessage}
+    onBotMessage={onSendBotMessage}
+    onConnect={onConnect}
+    onDisconnect={onDisconnect}
+  />;
+}
+
+export default App;
---
+++ src/chat-client.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import ListItemText from '@mui/material/ListItemText';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Paper from '@mui/material/Paper';
+import './chat-client.css';
+
+interface Props {
+  isConnected: boolean;
+  members: string[];
+  chatRows: React.ReactNode[];
+  onPublicMessage: () => void;
+  onPrivateMessage: (to: string) => void;
+  onConnect: () => void;
+  onDisconnect: () => void;
+  onBotMessage: () => void;
+}
+
+export const ChatClient = (props: Props) => {
+
+  return (
+    <div className='main-div'>
+      <CssBaseline />
+      <Container className='main-container'>
+        <Grid container style={{ height: '100%' }}>
+          {/** Grid com os contatos conectados */}
+          <Grid item xs={2} 
+            sx={{ borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px' }}
+            style={{ 
+              backgroundColor: '#800080 ',
+              color: 'white',
+              fontFamily: 'segoe UI',
+            }}
+            >
+            <List component="nav" style={{ marginRight: '20px' }}>
+              {props.members.map(item =>
+                <ListItem className='list-item-users' key={item} onClick={() => {{props.onPrivateMessage(item)};}}>
+                  <ListItemText style={{ fontWeight: 800, color: 'white' }} primary={item} />
+                </ListItem>
+              )}
+            </List>
+          </Grid>
+          {/* Grid das mensagens enviadas  */}
+          <Grid style={{ position: 'relative',}} item container direction="column" xs={10} >
+            <Paper style={{ minWidth:'12rem' ,flex: 1, backgroundColor: '#4b0082 ', borderRadius: '0px 10px 10px 0px' }}>
+              <Grid item container style={{ height: '100%' }} direction="column">
+                <Grid item container style={{ flex: 1 }}>
+                  <ul style={{
+                    paddingTop: 20,
+                    paddingLeft: 44,
+                    listStyleType: 'none',
+                  }}>
+                    {props.chatRows.map((item, i) =>
+                      <li key={i} className='message'>{item}</li>
+                    )}
+                  </ul>
+                </Grid>
+                {/* Botões de envio de mensagem */}
+                <Grid item style={{ margin: 10 }}>
+                  {props.isConnected && <Button style={{ marginTop: 10, marginRight: 7, color: "white", borderColor: "transparent", backgroundColor: '#7f00ff', fontWeight: 'bold'}} variant="outlined" size="small" disableElevation onClick={props.onPublicMessage}>Mensagem pública</Button>}
+                  {props.isConnected && <Button style={{ marginRight: 7, marginTop: 10, color: "white", borderColor: "transparent", backgroundColor: '#7f00ff', fontWeight: 'bold' }} variant="outlined" size="small" disableElevation onClick={props.onBotMessage}>Mensagem para o Bot</Button>}
+                  {props.isConnected && <Button style={{ marginRight: 7, marginTop: 10, color: "white", borderColor: "transparent", backgroundColor: '#c40000', fontWeight: 'bold'}} variant="outlined" size="small" disableElevation onClick={props.onDisconnect}>Sair</Button>}
+                  {!props.isConnected && <Button style={{ marginRight: 7, marginTop: 10, color: "white", borderColor: "transparent", backgroundColor: '#32cd32', fontWeight: 'bold'}} variant="outlined" size="small" disableElevation onClick={props.onConnect}>Entrar</Button>}
+                </Grid>
+              </Grid>
+              <div style={{ backgroundColor: props.isConnected ? '#00da00' : '#e2e2e2',}}
+              className='sec-div' />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div >
+  )
+};
